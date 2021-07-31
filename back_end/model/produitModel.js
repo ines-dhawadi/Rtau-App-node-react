dc0c7cbef5a1ebc541f340a2ac83880ac5dc62f6
ines-dhawadi <inesdhawadisimplonend@gmail.com>
2021-07-31 18:42:02 +0100

    get produit

--- back_end/model/produitModel.js
+++ back_end/model/produitModel.js
@@ -5,19 +5,19 @@ const Schema = mongoose.Schema
 const produitSchema = new Schema({
     titre:{
      type: String,
-     required : true
+     
  },
  image:{
     type: String,
-    required : true
+   
 },
 desc:{
     type: String,
-    required : true
+  
 },
 prix:{
     type: String,
-    required : true
+   
 }
 
 })
